d5ddc6ae55defdf412023ed11fbb288135093155
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro-2.local>
2020-05-26 08:56:08 -0600

    pre-release commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import AnimatedIcon from './src/AnimatedIcon';
-import * as icons from './icons';
 import registry, { get, register, remove, reset } from './src/registry';
 
 export {
-    icons,
     get,
     registry,
     register,
