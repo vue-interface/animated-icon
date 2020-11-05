bb6e13d385bfab9731c208f81bf24cb5a9ab064e
Hostinger <hello@mntarts.org>
2020-11-05 07:33:07 -0700

    Updates

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     presets: [
-        ['@vue/app', {
-            targets: {
-                node: "current",
-                browsers: "> 0.25%, not dead"
-            }
+        ['@vue/cli-plugin-babel/preset', {
+            browsers: [
+                "> 1%",
+                "last 2 versions",
+                "not dead"
+            ]
         }]
     ]
 };
--- src/registry.js
+++ src/registry.js
@@ -2,21 +2,10 @@ import IconRegistry from './IconRegistry';
 
 const registry = new IconRegistry();
 
-function get() {
-    return registry.get.apply(registry, arguments);
-}
-
-function register() {
-    return registry.register.apply(registry, arguments);
-}
-
-function remove() {
-    return registry.remove.apply(registry, arguments);
-}
-
-function reset() {
-    return registry.reset.apply(registry, arguments);
-}
+const get = registry.get;
+const register = registry.register;
+const remove = registry.remove;
+const reset = registry.reset;
 
 export {
     get,
