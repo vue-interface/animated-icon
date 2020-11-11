82f224ce35cee4cd82e50fd9321060ae9d9c9176
Justin Kimbrell <justinkimbrell@Justins-MacBook-Pro.local>
2020-11-11 14:16:29 -0700

    pre-release commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import AnimatedIcon from './src/AnimatedIcon';
-import { registry, get, register, remove, reset } from './src/registry';
+
+export * from './src/registry';
 
 export {
-    AnimatedIcon,
-    get,
-    registry,
-    register,
-    remove,
-    reset
+    AnimatedIcon
 };
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        test
+    </div>
+</template>
+
+<script>
+import { AnimatedIcon } from '..';
+
+export default {
+    components: {
+        
+    }    
+};
+</script>
